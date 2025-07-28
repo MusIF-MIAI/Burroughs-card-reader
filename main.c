efceba1f08b8393dacba117504a7a98e528858bf
Daniele Lacamera <root@danielinux.net>
2025-07-28 21:40:57 +0200

    Cards are now aligned to photodiode #13

--- main.c
+++ main.c
@@ -6,15 +6,13 @@
 
 #define NUM_COLUMNS 80
 #define NUM_DATA_PINS 12
-#define START_DELAY_US 9000
-#define COLUMN_READ_DELAY_US 3400 //3400 was good
 
 #define COLUMNS 80
 #define CARD_CLOCKS 684
 
 #define BITMASK (0x00001BFF)
 
-#define START_COUNT 23
+#define START_COUNT 4 
 #define COLUMN_READ_COUNT 8
 
 static repeating_timer_t sampler_tmr;
@@ -26,37 +24,46 @@ static uint32_t card_time[MAX_CARDS];
 static volatile int n_cards = 0;
 static volatile uint32_t jiffies = 0;
 
-bool card_inserted() {
-    // Wait until all 12 pins go low (0) = blocking light (card inserted)
-    uint16_t pin_state = gpio_get_all() & BITMASK;
-    return pin_state != BITMASK;
-}
+static volatile unsigned int sample_clock_count = 0;
 
-bool card_finished() {
-    // Wait until all 12 pins go low (0) = blocking light (card inserted)
-    uint16_t pin_state = gpio_get_all() & BITMASK;
-    return pin_state == BITMASK;
-}
+static volatile unsigned int card_inserted = 0;
 
-static volatile unsigned int sample_clock_count = START_COUNT;
 
-// Interrupt handler function
-void clock_callback(uint gpio, uint32_t events) {
-    jiffies++;
-    if (col_ctr >= COLUMNS)
-        sample_clock_count = 0;
-    if (sample_clock_count == 0)
+void gpio_callback(uint gpio, uint32_t events)
+{
+    if (gpio == 18) {
+        jiffies++;
+        if (!card_inserted)
+            return;
+        if (col_ctr >= COLUMNS) {
+            sample_clock_count = 0;
+        }
+        if (sample_clock_count == 0)
+            return;
+        if (--sample_clock_count == 0) {
+            sample[n_cards][col_ctr++] = (uint16_t)(gpio_get_all() & BITMASK);
+            sample_clock_count = COLUMN_READ_COUNT;
+        }
         return;
-    if (--sample_clock_count == 0) {
-        sample[n_cards][col_ctr++] = (uint16_t)(gpio_get_all() & BITMASK);
-        sample_clock_count = COLUMN_READ_COUNT;
+    }
+    if ((events & GPIO_IRQ_EDGE_FALL) && (card_inserted == 0)) {
+        card_time[n_cards] = jiffies;
+        col_ctr = 0;
+        sample_clock_count = START_COUNT;
+        card_inserted = 1;
+        return;
+    }
+    if ((events & GPIO_IRQ_EDGE_RISE) && (card_inserted == 1)) {
+        card_time[n_cards++] = jiffies;
+        card_inserted = 0;
+        jiffies = 0;
     }
 }
 
 int main() {
     unsigned int i, j, card;
     stdio_init_all();
-    sleep_ms(1300);
+    sleep_ms(2000);
 
     // Initialize GPIOs 0 through 11 as inputs
     for (i = 0; i < NUM_DATA_PINS; ++i) {
@@ -70,13 +77,15 @@ int main() {
     gpio_set_dir(19, GPIO_IN);
     gpio_pull_down(18);  // ensure low when no signal
     gpio_pull_down(19);  // ensure low when no signal
+    gpio_set_irq_enabled_with_callback(18, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(19, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     printf("Waiting for card...\n");
 
     while (1) {
         char c;
         // Wait for card to be inserted (all pins 0)
-        while (!card_inserted()) {
+        while (!card_inserted) {
             c = getchar_timeout_us(0);
             if (c == ' ') {
                 for (card = 0; card < n_cards; card++) {
@@ -114,21 +123,7 @@ int main() {
                 printf("\n\n");
             }
         }
-
-        sample_clock_count = START_COUNT;
-        gpio_set_irq_enabled_with_callback(18, GPIO_IRQ_EDGE_RISE, true, &clock_callback);
-
-        /* Do nothing. Sampling happens in callbacks from timers */
-
-        while(gpio_get(19) == 0)
-            ;
-
-        card_time[n_cards] = jiffies;
-        n_cards++;
-        jiffies = 0;
-        //gpio_set_irq_enabled(18, GPIO_IRQ_EDGE_RISE, false);
-
-        col_ctr = 0;
+        asm volatile("wfi");
     }
     return 0;
 }
